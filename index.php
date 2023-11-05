cd87d5ab724ba10059c9e95166ad747f73c2bbee
MYUSUP99 <132921522+MYUSUP99@users.noreply.github.com>
2023-11-05 09:59:21 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,49 @@
+<?php
+// require 'functions/functions.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dashboard</title>
+    <title>Login</title>
+
     <style>
+        .mt {
+            margin-top: 20px;
+        }
+        
         a {
-            display: flex;
-            margin: 0px;
-            padding-left: 20px;
-            font-size: 16px;
+            margin-top: 15px;
+            background-color: #000;
+            padding: 5px 5px;
+            padding-left: 10px;
+            padding-right: 10px;
             text-decoration: none;
+            color: white;
+            border-radius: 3px;
         }
     </style>
 </head>
 <body>
+    
+   <center>
+        <h3>Koperasi Pegawai RSUD BINTARO</h3>
+        <img src="oke.png"style="width:500px;height:400px;">
+        <h2>Login</h2>
 
-    <div>
-        <h2>Selamat Datang, Admin</h2>
-    </div>
-    <div>
-        <a href="customer.php ">Customer</a><br>
-        <a href="item.php">Item</a><br>
-        <a href="identitas.php">Identitas</a><br>
-        <a href="sales.php">Sales</a><br>
-        <a href="transuctions.php">Transaction</a><br>
-        <a href="../index.php">Logout</a><br>
-    </div>
+        <form action="admin/index.php" method="post">
+            <input type="text" name="" placeholder="masukan username">
+            
+            <input type="text" name="" placeholder="masukan password">
+        </form>
+
+        <div class="mt">
+            <a href="admin/index.php" name="submit" type=" button">Login</a>
+        </div>
+   </center>
 </body>
 </html>
